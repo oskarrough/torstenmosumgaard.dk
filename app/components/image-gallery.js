c05375626eddcfda22a089842b4bae28380368d6
Oskar Rough <oskarrough@gmail.com>
2016-11-12 23:19:37 +0100

    Add previous/next buttons

--- app/components/image-gallery.js
+++ app/components/image-gallery.js
@@ -1,9 +1,10 @@
 import Ember from 'ember';
 
-const {Component, set} = Ember;
+const {Component, get, set} = Ember;
 
 export default Component.extend({
   tagName: 'section',
+  // images
 
   actions: {
     open(img) {
@@ -11,6 +12,16 @@ export default Component.extend({
     },
     close() {
       set(this, 'selectedImage', '');
+    },
+    next() {
+      const index = get(this, 'images').indexOf(get(this, 'selectedImage'));
+      console.log(index);
+      set(this, 'selectedImage', get(this, 'images').objectAt(index + 1));
+    },
+    previous() {
+      const index = get(this, 'images').indexOf(get(this, 'selectedImage'));
+      console.log(index);
+      set(this, 'selectedImage', get(this, 'images').objectAt(index - 1));
     }
   }
 });
