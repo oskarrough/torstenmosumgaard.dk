f7a656c6b3b09cab7dd84c602711a2f1974af120
Oskar Rough <oskar@rough.dk>
2016-11-13 12:09:23 +0100

    Add query params for the images

--- app/components/image-gallery.js
+++ app/components/image-gallery.js
@@ -3,25 +3,36 @@ import Ember from 'ember';
 const {Component, get, set} = Ember;
 
 export default Component.extend({
-  tagName: 'section',
-  // images
+  didReceiveAttrs() {
+    this._super(...arguments);
+    // If there is an id, open the modal.
+    const id = get(this, 'selectedImageId');
+    if (id) {
+      const img = get(this, 'images').filterBy('public_id', id)[0];
+      this.setImage(img);
+    }
+  },
+
+  setImage(img) {
+    set(this, 'selectedImage', img);
+    set(this, 'selectedImageId', img.public_id);
+  },
 
   actions: {
     open(img) {
-      set(this, 'selectedImage', img);
+      this.setImage(img);
     },
     close() {
       set(this, 'selectedImage', '');
+      set(this, 'selectedImageId', '');
     },
     next() {
       const index = get(this, 'images').indexOf(get(this, 'selectedImage'));
-      console.log(index);
-      set(this, 'selectedImage', get(this, 'images').objectAt(index + 1));
+      this.setImage(get(this, 'images').objectAt(index + 1));
     },
     previous() {
       const index = get(this, 'images').indexOf(get(this, 'selectedImage'));
-      console.log(index);
-      set(this, 'selectedImage', get(this, 'images').objectAt(index - 1));
+      this.setImage(get(this, 'images').objectAt(index - 1));
     }
   }
 });
