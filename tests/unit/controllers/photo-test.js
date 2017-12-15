e698cc88c85382e42b0f01c3a8c58d4bb5717574
Oskar Rough <oskar@rough.dk>
2017-12-15 01:24:59 +0100

    Fix tests

--- tests/unit/controllers/photo-test.js
+++ tests/unit/controllers/photo-test.js
@@ -2,7 +2,7 @@ import {moduleFor, test} from 'ember-qunit'
 
 moduleFor('controller:photo', 'Unit | Controller | photo', {
 	// Specify the other units that are required for this test.
-	// needs: ['controller:foo']
+	needs: ['service:store']
 })
 
 // Replace this with your real tests.
