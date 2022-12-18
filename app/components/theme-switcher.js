1bdfde17a8cb282d298de2db53d6784acd4b29d2
Oskar Rough <oskar@rough.dk>
2022-12-18 23:50:34 +0100

    Replace Ember with mini version

--- app/components/theme-switcher.js
+++
@@ -1,9 +0,0 @@
-import Component from '@ember/component'
-
-export default Component.extend({
-	tagName: 'button',
-	classNames: ['ThemeSwitcher'],
-	click() {
-		document.body.classList.toggle('theme-dark')
-	}
-})
--- app/routes/photo.js
+++
@@ -1,8 +0,0 @@
-import Ember from 'ember'
-
-export default Ember.Route.extend({
-	model(params) {
-		const photos = this.modelFor('application')
-		return photos.filterBy('id', params.photo_id)[0]
-	}
-})
